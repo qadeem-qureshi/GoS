49a6aa9f2ec1f4e2bf2daa23286b1ec8f5195151
Cloudhax23 <qadeem@aplabs.us>
2015-08-08 11:21:14 -0500

    Create lel.lua

---
+++ lel.lua
@@ -0,0 +1,24 @@
+require 'MapPositionGOS'
+ Config = scriptConfig("Gnar2", "Gnar2")
+ Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+OnLoop(function(myHero)
+        AutoRiAC()
+end)
+ 
+-- modify from IAC vayne
+function AutoRiAC()
+        local target = GetCurrentTarget()
+        if ValidTarget(target,GetCastRange(myHero,_E)) and IsInDistance(target, GetCastRange(myHero,_R)) and CanUseSpell(myHero,_R) == READY then
+                for _=0,450,GetHitBox(target) do
+                        local Pred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target), 2000, 250, 1000, 1, true, true)
+            local tPos = Vector(Pred.PredPos)+Vector(Vector(Pred.PredPos)-Vector(myHero)):normalized()*_
+            if MapPosition:inWall(tPos) then
+              CastSkillShot(_R,Pred.PredPos.x,Pred.PredPos.y,Pred.PredPos.z)
+              break
+            end
+          end
+        end
+end
+
+
+-- Credits: Ilovesona Inspired Maxxxel
