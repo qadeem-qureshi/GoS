766548167fe50ba0fb6ff21abb2d6575c487408e
Cloudhax23 <qadeem@aplabs.us>
2015-08-14 17:39:29 -0500

    Update lel.lua

--- lel.lua
+++ lel.lua
@@ -1,14 +1,17 @@
 require 'MapPositionGOS'
  Config = scriptConfig("Gnar2", "Gnar2")
  Config.addParam("R", "Use R", SCRIPT_PARAM_ONOFF, true)
+ Config.addParam("Combo", "Combo", SCRIPT_PARAM_KEYDOWN, string.byte(" "))
 OnLoop(function(myHero)
         AutoRiAC()
 end)
  
 -- modify from IAC vayne
 function AutoRiAC()
+  if Config.Combo then
+  if Config.R then 
         local target = GetCurrentTarget()
-        if ValidTarget(target,GetCastRange(myHero,_E)) and IsInDistance(target, GetCastRange(myHero,_R)) and CanUseSpell(myHero,_R) == READY then
+        if ValidTarget(target,GetCastRange(myHero,_R)) and IsInDistance(target, GetCastRange(myHero,_R)) and CanUseSpell(myHero,_R) == READY then
                 for _=0,450,GetHitBox(target) do
                         local Pred = GetPredictionForPlayer(GetMyHeroPos(),target,GetMoveSpeed(target), 2000, 250, 1000, 1, true, true)
             local tPos = Vector(Pred.PredPos)+Vector(Vector(Pred.PredPos)-Vector(myHero)):normalized()*_
@@ -18,7 +21,7 @@ function AutoRiAC()
             end
           end
         end
+      end
+    end
 end
-
-
 -- Credits: Ilovesona Inspired Maxxxel
