004c60d2ae11b674aca1ca9106ed600ee45feee2
Cloudhax23 <qadeem@aplabs.us>
2015-08-30 09:40:55 -0500

    Create CloudBot.lua

---
+++ Common/CloudBot.lua
@@ -0,0 +1,75 @@
+myIAC = IAC()
+priority = {} 
+unit = GetCurrentTarget()
+Config = scriptConfig("CB", "CloudBot")
+OnLoop(function(myHero)
+Farm()
+HitFarm()
+AtkE()
+
+JungleFarm()
+for _, ally1 in pairs (GetAllyHeroes()) do
+        local Allyclosest = AllyNearMe(myHeroPos)
+        local Ally = GetOrigin(Allyclosest)
+        ally2 = GetOrigin(ally1)
+if GetDistance(Allyclosest, myHero) > 300 and IsDead(myHero) == false then
+  MoveToXYZ(Ally.x+100,Ally.y,Ally.z-261)
+end
+end
+end)
+
+
+
+function AllyNearMe(pos)
+    local ally = nil
+    for k,v in pairs(GetAllyHeroes()) do 
+        if not ally and v then ally = v end
+        if ally and v and GetDistanceSqr(GetOrigin(ally),pos) > GetDistanceSqr(GetOrigin(v),pos) then
+            ally = v
+        end
+    end
+    return ally 
+end
+
+function Farm()
+ for _,k in pairs(GetAllMinions(MINION_ENEMY)) do
+        local targetPos = GetOrigin(k)
+        local drawPos = WorldToScreen(1,targetPos.x,targetPos.y,targetPos.z)
+        local hp = GetCurrentHP(k)
+        local dmg = CalcDamage(myHero, k, 50+GetBonusDmg(myHero)+GetBaseDamage(myHero))
+        if dmg > hp and ValidTarget(k, 500) and GetDistance(myHero, ally1) < 450 then
+        AttackUnit(k)
+end
+end
+end
+function HitFarm()
+   for _,k in pairs(GetAllMinions(MINION_ENEMY)) do
+    if ValidTarget(k, 600) and GetDistance(myHero, ally1) > 750 and EnemiesAround(GetOrigin(myHero), 600) == 0 then
+        AttackUnit(k)
+end
+end
+end
+function JungleFarm()
+   for _,k in pairs(GetAllMinions(MINION_JUNGLE)) do
+    if ValidTarget(k, 500) and GetDistance(myHero, ally1) < 450 then
+        AttackUnit(k)
+end
+end
+end
+function AtkE()
+for i,enemy in pairs(GetEnemyHeroes()) do
+if ValidTarget(enemy, 500) and GetDistance(myHero, ally1) < 450 then
+  AttackUnit(enemy)
+end
+end
+end
+function turrentattack()
+if IsInDistance(Obj_AI_Turret, 1000) and ValidTarget(Obje_AI_Turrent, 700) and GetDistance(myHero, ally1) > 600 then
+  AttackUnit(Obj_AI_Turret)
+end
+end
+function barrackattack()
+if IsInDistance(Obj_AI_Barracks, 1000) and ValidTarget(Obj_AI_Barracks, 700) and GetDistance(myHero, ally1) > 600 then
+  AttackUnit(Obj_AI_Barracks)
+end
+end
