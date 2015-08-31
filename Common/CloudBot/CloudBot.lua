6e217f2476b89d4fd48b5522455b967bd5c649c8
Cloudhax23 <qadeem@aplabs.us>
2015-08-31 18:57:46 -0500

    Update CloudBot.lua

--- Common/CloudBot/CloudBot.lua
+++ Common/CloudBot/CloudBot.lua
@@ -1,7 +1,14 @@
 myIAC = IAC()
-priority = {} 
+priority = {}
 unit = GetCurrentTarget()
 Config = scriptConfig("CB", "CloudBot")
+Config.addParam("U", "Jungle Clear", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("I", "AutoFollow", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("F", "Hit Inhibs", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("S", "Hit Turrents", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("D", "Hit Enemies", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("O", "LastHit", SCRIPT_PARAM_ONOFF, true)
+Config.addParam("V", "LaneClear", SCRIPT_PARAM_ONOFF, true)
 OnLoop(function(myHero)
 Farm()
 HitFarm()
@@ -9,14 +16,17 @@ AtkE()
 JungleFarm()
 turrentattack()
 barrackattack()
+
 for _, ally1 in pairs (GetAllyHeroes()) do
         local Allyclosest = AllyNearMe(myHeroPos)
-        local Ally = GetOrigin(Allyclosest)
+        Ally = GetOrigin(Allyclosest)
         ally2 = GetOrigin(ally1)
-if GetDistance(Allyclosest, myHero) > 300 and IsDead(ally1) == false and (GetCurrentHP(myHero)/GetMaxHP(myHero))>.20 and GotBuff(ally1, "recall") == 0 then
+if GetDistance(Allyclosest, myHero) > 300 and IsDead(ally1) == false and (GetCurrentHP(myHero)/GetMaxHP(myHero))>.20 and GotBuff(ally1, "recall") == 0 and Config.U then
 MoveToXYZ(Ally.x+100,Ally.y,Ally.z+190)
-elseif (GetCurrentHP(myHero)/GetMaxHP(myHero))<.20 then
+elseif (GetCurrentHP(myHero)/GetMaxHP(myHero))<.20 and GetTeam(myHero) == 100 then
 MoveToXYZ(12,0,30)
+elseif (GetCurrentHP(myHero)/GetMaxHP(myHero))<.20 and GetTeam(myHero) == 200 then
+MoveToXYZ(400, 200, 400)
 end
 end
 end)
@@ -38,39 +48,48 @@ function Farm()
         local drawPos = WorldToScreen(1,targetPos.x,targetPos.y,targetPos.z)
         local hp = GetCurrentHP(k)
         local dmg = CalcDamage(myHero, k, 50+GetBonusDmg(myHero)+GetBaseDamage(myHero))
-        if dmg > hp and ValidTarget(k, 500) and GetDistance(myHero, ally1) < 450 then
+        if dmg > hp and ValidTarget(k, 500) and GetDistance(myHero, Ally) <= 550 and Config.O then
         AttackUnit(k)
 end
 end
 end
+
 function HitFarm()
    for _,k in pairs(GetAllMinions(MINION_ENEMY)) do
-    if ValidTarget(k, 600) and GetDistance(myHero, ally1) < 750 and EnemiesAround(GetOrigin(myHero), 850) == 0 then
+    if ValidTarget(k, 600) and GetDistance(myHero, Ally) <= 500 and EnemiesAround(GetOrigin(myHero), 850) == 0 and Config.V then
         AttackUnit(k)
 end
 end
 end
+
 function JungleFarm()
    for _,k in pairs(GetAllMinions(MINION_JUNGLE)) do
-    if ValidTarget(k, 500) and GetDistance(myHero, ally1) > 650 then
+    if ValidTarget(k, 500) and GetDistance(myHero, Ally) <= 500 and Config.U then
         AttackUnit(k)
 end
 end
 end
+
 function AtkE()
 for i,enemy in pairs(GetEnemyHeroes()) do
-if ValidTarget(enemy, 500) and GetDistance(myHero, ally1) < 650 then
+if ValidTarget(enemy, 500) and GetDistance(myHero, Ally) <= 500 and EnemiesAround(GetOrigin(Obj_AI_Turrent), 800) == 0 and Config.D then
   AttackUnit(enemy)
 end
 end
 end
+
 function turrentattack()
-if  ValidTarget(Obj_AI_Turrent, 700) and GetDistance(myHero, ally1) < 650 then
-  AttackUnit(Obj_AI_Turret)
+for i,turrent in pairs(objectManager.turrets) do
+if ValidTarget(turrent, 600) and GetDistance(myHero, Ally) <= 550 and EnemiesAround(GetOrigin(turrent), 800) == 0 and Config.S then
+  AttackUnit(turrent)
 end
 end
+end
+
 function barrackattack()
-if ValidTarget(Obj_AI_Barracks, 700) and GetDistance(myHero, ally1) < 600 then
-  AttackUnit(Obj_AI_Barracks)
+for i,barrack in pairs(objectManager.barracks) do
+if ValidTarget(barrack, 600) and GetDistance(myHero, Ally) <= 550 and EnemiesAround(GetOrigin(barrack), 800) == 0 and Config.F then
+  AttackUnit(barrack)
+end
 end
 end
