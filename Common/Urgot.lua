ea26bdebd24d46969ce914ea221eb23bd0476c24
Cloudhax23 <qadeem@netsyms.com>
2016-02-06 09:23:42 -0600

    Update Urgot.lua

--- Common/Urgot.lua
+++ Common/Urgot.lua
@@ -14,8 +14,8 @@ Config.c:Slider("hQ", "HitChance Q", 20, 0, 100, 1)
   Callback.Add("Tick", function() Loop() end)
 function Loop()
 	local unit = GetCurrentTarget()
-	local EPred = GetCircularAOEPrediction(unit, EPred, myHero)
-	local QPred = GetPrediction(unit, QPred, myHero)
+	local EPred = GetCircularAOEPrediction(unit, EPred) -- Filthy Zwei PogChamp
+	local QPred = GetPrediction(unit, QPred) -- Filthy Zwei PogChamp
 	if IOW:Mode() == "Combo" then
 		if Config.c.Q2:Value() and CanUseSpell(myHero, _E) and EPred and EPred.hitChance >= (Config.c.hE:Value()/100) and not EPred:mCollision(1) then
 			CastSkillShot(_E, EPred.castPos)
@@ -23,7 +23,7 @@ function Loop()
 		if Config.c.Q1:Value() and CanUseSpell(myHero, _Q) and QPred and QPred.hitChance >= (Config.c.hQ:Value()/100) and GotBuff(unit, "urgotcorrosivedebuff") == 0 and not QPred:mCollision(1) then
 			CastSkillShot(_Q, QPred.castPos)
 			elseif Config.c.Q1:Value() and CanUseSpell(myHero, _Q) and GotBuff(unit, "urgotcorrosivedebuff") >= 1 then
-				CastSkillShot(_Q, unit)
+				CastSkillShot(_Q, GetOrigin(unit)) -- Filthy Zwei PogChamp
 		end
 	end
 end
