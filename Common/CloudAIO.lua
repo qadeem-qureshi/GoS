bda66e4a2843ee19e27955dd57898acf47082f0a
Cloudhax23 <qadeem@netsyms.com>
2016-04-01 07:33:46 -0500

    Update CloudAIO.lua

--- Common/CloudAIO.lua
+++ Common/CloudAIO.lua
@@ -1,2 +1,3 @@
 assert(loadstring(Base64Decode("G0x1YVEAAQQEBAgADgAAAEBDbG91ZEFJTy5sdWEAAAAAAAAAAAAAAAICAQAAAB4AgAAAAAAAAAAAAAEAAAABAAAAAAAAAAAAAAA="), nil, "bt", _ENV))()
 --CloudAIO downloader
+print("Dont spoil the joke for other users")
